e4fd1d3e65ba8d64b15a00143f8145e565f28697
Chandrahasa Reddy <chandrahasa.reddy@rbc.com>
2018-07-03 16:41:10 -0400

    First commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { StylesCompileDependency } from '@angular/compiler';
 
 @Component({
     selector:'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color:white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: String = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online':'offline';
+    }
     getServerStatus(){
         return this.serverStatus;
     }
-
+    getColor(){
+        return this.serverStatus === 'online' ?'green':'red';
+    }
 }
